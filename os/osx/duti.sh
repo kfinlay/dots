597012eac8ea980a76d12aa22e438620253dcc10
Keith Finlay <kfinlay@gmail.com>
2014-12-16 15:50:19 -0600

    st3 for duti

--- os/osx/duti.sh
+++ os/osx/duti.sh
@@ -5,7 +5,7 @@
 if [ "$USER" = "kfinlay" ]
 then
 	# gem install xcode-installer
-	
+
 	if test ! $(which duti); then
 	  echo "Installing duti..."
 	  brew install duti
@@ -14,7 +14,7 @@ then
 	duti -s net.sourceforge.skim-app.skim pdf all
 	for EXT in ado cdb css do htm html json log m macosx mata osx php py r rb sh shtm shtml sty tex tm_properties txt xml
 	do
-		duti -s com.macromates.TextMate.preview $EXT all
+		duti -s com.sublimetext.3 $EXT all
 	done
 	for EXT in csv dat
 	do
