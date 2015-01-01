2422fb2c2389496bd05601cd861be96e121235d2
Keith Finlay <kfinlay@gmail.com>
2015-01-01 11:30:03 -0600

    duti addition

--- os/osx/duti.sh
+++ os/osx/duti.sh
@@ -12,7 +12,7 @@ then
 	fi
 
 	duti -s net.sourceforge.skim-app.skim pdf all
-	for EXT in ado cdb css do htm html json log m macosx mata osx php py r rb sh shtm shtml sty tex tm_properties txt xml
+	for EXT in ado cdb css do htm html json log m macosx mata md mdown markdown osx php py r rb sh shtm shtml statalog sty tex tm_properties txt xml
 	do
 		duti -s com.sublimetext.3 $EXT all
 	done
