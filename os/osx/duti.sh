ad4175d96c8259fa5918de98238035f5ff008d4a
Keith Finlay <kfinlay@gmail.com>
2015-01-07 13:17:03 -0600

    sublime text exts

--- os/osx/duti.sh
+++ os/osx/duti.sh
@@ -12,7 +12,7 @@ then
 	fi
 
 	duti -s net.sourceforge.skim-app.skim pdf all
-	for EXT in ado cdb css do htm html json log m macosx mata md mdown markdown osx php py r rb sh shtm shtml statalog sty tex tm_properties txt xml
+	for EXT in cdb css htm html json log m macosx mata md mdown markdown osx php py r rb sh shtm shtml statalog sty tex tm_properties txt xml
 	do
 		duti -s com.sublimetext.3 $EXT all
 	done
@@ -20,6 +20,11 @@ then
 	do
 		duti -s com.barebones.TextWrangler.TextWrangler $EXT all
 	done
+	for EXT in ado do
+	do
+		duti -s com.stata.stata13 $EXT all
+	done
+
 fi
 
 exit 0
