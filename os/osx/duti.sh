c525c719642670dff57cd882c5ebc91b65b2ecfb
Keith Finlay <kfinlay@gmail.com>
2015-01-09 15:28:03 -0600

    duti changes

--- os/osx/duti.sh
+++ os/osx/duti.sh
@@ -11,8 +11,11 @@ then
 	  brew install duti
 	fi
 
-	duti -s net.sourceforge.skim-app.skim pdf all
-	for EXT in cdb css htm html json log m macosx mata md mdown markdown osx php py r rb sh shtm shtml statalog sty tex tm_properties txt xml
+	for EXT in pdf
+	do
+		duti -s net.sourceforge.skim-app.skim $EXT all
+	done
+	for EXT in bib cdb css htm html json log m macosx md mdown markdown osx php py r rb ris sh shtm shtml statalog sty tex tm_properties txt xml
 	do
 		duti -s com.sublimetext.3 $EXT all
 	done
@@ -20,9 +23,13 @@ then
 	do
 		duti -s com.barebones.TextWrangler.TextWrangler $EXT all
 	done
-	for EXT in ado do
+	for EXT in ado do mata
 	do
 		duti -s com.stata.stata13 $EXT all
+	done
+	for EXT in avi wav
+	do
+		duti -s org.videolan.vlc $EXT all
 	done
 
 fi
